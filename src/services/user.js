2e32eca10a09f065adf26fb627a4addb39d401b7
EmilyDSarani <emily.sarani@gmail.com>
2022-02-28 11:09:50 -0600

    adding the api/v1 to all the user routes for services

--- src/services/user.js
+++ src/services/user.js
@@ -4,21 +4,21 @@
 
 //fetch All
 export const getAllUsers = async () => {
-    const res = await fetch('https://bop-simon.herokuapp.com/users');
+    const res = await fetch('https://bop-simon.herokuapp.com/api/v1/users');
     const users = await res.json();
     return users
 }
 
 //fetch by Id
 export const getUserById = async(id) => {
-    const res = await fetch(`https://bop-simon.herokuapp.com/users/${id}`); 
+    const res = await fetch(`https://bop-simon.herokuapp.com/api/v1/users/${id}`); 
     const userId = await res.json();
     return userId
 }
 
 //fetch by score
 export const getHighScores = async(id) => {
-    const res = await fetch(`https://bop-simon.herokuapp.com/users/${leaderboard}`); 
+    const res = await fetch(`https://bop-simon.herokuapp.com/api/v1/users/${leaderboard}`); 
     const userId = await res.json();
     return userId
 }
