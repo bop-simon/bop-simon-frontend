5d868a5de0842d9429229e086a04d598e0ff4a22
EmilyDSarani <emily.sarani@gmail.com>
2022-02-27 19:23:40 -0600

    user services mapped out

--- src/services/user.js
+++ src/services/user.js
@@ -4,15 +4,21 @@
 
 //fetch All
 export const getAllUsers = async () => {
-    const res = await fetch('https://bop-simon.herokuapp.com/');
+    const res = await fetch('https://bop-simon.herokuapp.com/users');
     const users = await res.json();
     return users
 }
 
 //fetch by Id
 export const getUsersById = async(id) => {
-    const res = await fetch(`https://bop-simon.herokuapp.com/${id}`); 
+    const res = await fetch(`https://bop-simon.herokuapp.com/users/${id}`); 
     const userId = await res.json();
     return userId
+}
 
+//fetch by score
+export const getHighScores = async(id) => {
+    const res = await fetch(`https://bop-simon.herokuapp.com/users/${leaderboard}`); 
+    const userId = await res.json();
+    return userId
 }
