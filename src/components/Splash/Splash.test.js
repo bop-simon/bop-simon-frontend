0d1383c3b27db546c869cfda6cdb7a783653215d
Taylor Callan Williams <taylor.callan.williams@gmail.com>
2022-02-28 10:52:50 -0800

    memory router test fix

--- src/components/Splash/Splash.test.js
+++ src/components/Splash/Splash.test.js
@@ -1,8 +1,12 @@
 import { render } from '@testing-library/react'
 import Splash from './Splash'
+import { MemoryRouter } from 'react-router-dom'
 
 it('properly renders the splash view', () => {
-  const container = render(<Splash />)
-
+  const container = render(
+    <MemoryRouter>
+      <Splash />
+    </MemoryRouter>
+  )
   expect(container).toMatchSnapshot()
 })
