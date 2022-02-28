fb8a9de2a13f09eb7dbfdfcfcfb8ba21cab71f8d
Taylor Callan Williams <taylor.callan.williams@gmail.com>
2022-02-27 16:59:00 -0800

    useHref bug fix, splash snap test passing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
 import RoutesComponent from './components/RoutesComponent/RoutesComponent'
 import Theme from './context/Theme.js'
-
+import MenuBar from './components/MenuBar/MenuBar'
 export default function App() {
-	return (
-		<>
-			<ThemeProvider theme={Theme}>
-				<BrowserRouter>
-					<RoutesComponent />
-				</BrowserRouter>
-			</ThemeProvider>
-		</>
-	)
+  return (
+    <>
+      <ThemeProvider theme={Theme}>
+        <BrowserRouter>
+          <MenuBar />
+          <Routes>
+            <RoutesComponent />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  )
 }
--- src/components/Splash/Splash.test.js
+++ src/components/Splash/Splash.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 import Splash from './Splash'
 
 it('properly renders the splash view', () => {
-  const app = render(<Splash />)
+  const container = render(<Splash />)
 
   expect(container).toMatchSnapshot()
 })
