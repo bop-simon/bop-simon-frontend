feb8545de57551664ba67771201f18142ec3b05c
EmilyDSarani <emily.sarani@gmail.com>
2022-02-28 16:01:45 -0600

    profile.jsx started planning

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,8 +1,18 @@
 import MenuBar from '../../components/MenuBar/MenuBar'
 import styles from './profile.css'
 import simon from '../../assets/simon.png'
+// import useUser from '../../context/UserContext'
 
 export default function About() {
+  //const {user, setUser} = useUser()
+
+  //function PasswordUpdate(){
+  // grab the user id and change the password associated with that user
+  //this would probably take in a patch/put, check the backend and create that route in user.js
+  //then return the updated user
+  //}
+
+
   return (
     <>
       {/* <MenuBar /> */}
