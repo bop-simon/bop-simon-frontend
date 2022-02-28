b786e2131dfdd1f67dbaf34e8a9bc121a34fca44
Katie Schrattenholzer <ktschr11@gmail.com>
2022-02-28 13:38:26 -0800

    update for CI - wrap test for splash in memory router

--- src/views/Splash/Splash.test.js
+++ src/views/Splash/Splash.test.js
@@ -1,8 +1,9 @@
 import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import Splash from './Splash'
 
 it('properly renders the splash view', () => {
-  const app = render(<Splash />)
+  const app = render(<MemoryRouter><Splash /></MemoryRouter>)
 
   expect(container).toMatchSnapshot()
 })
