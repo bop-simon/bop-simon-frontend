4a5b95e9218e03f5e49e83f910729828ef234e6b
EmilyDSarani <emily.sarani@gmail.com>
2022-03-01 15:53:23 -0600

    okay, cleaned up the ternary and have the userContext completed

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,11 +4,13 @@ import { getUserById } from "../services/user";
 const UserContext = createContext()
 
 const UserProvider = ({children}) =>{
-
+    //this is the state
     const [user, setUser] = useState({});
+    //current user is the getId function, which takes in an id
     const currentUser = getUserById(id)
-    const userId = currentUser ? { id: currentUser.id} : {}
-
+    //userId is-- if it is the getId function return the id else return empty object
+    const userId = currentUser ? { id: currentUser} : {}
+    //setUser to the userId
     setUser(userId);
 
     return <UserContext.UserProvider value={{user, setUser}}>{children}</UserContext.UserProvider>
