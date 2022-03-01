4eddc86e4dc718de5db69a33069d5e7bb3f5f776
EmilyDSarani <emily.sarani@gmail.com>
2022-03-01 12:03:27 -0600

    services almost done

---
+++ src/services/client.js
@@ -0,0 +1,17 @@
+//create client
+export const client = async () => {
+    const res = await fetch('https://bop-simon.herokuapp.com/api/v1/users/sessions')
+    const clients = await res.json();
+    return clients
+    }
+
+//export const client = createClient(
+//process.env.HEROKU_URL,
+//process.env.HEROKU_KEY
+//);
+
+    export const parseData = ({ data, error }) => {
+        if (error) throw error;
+      
+        return data;
+      };
