3c648608ba7bcf1e7b52cbddaa4f46d8164bbca6
EmilyDSarani <emily.sarani@gmail.com>
2022-03-01 15:57:58 -0600

    fixed getbyhighscore

--- src/services/user.js
+++ src/services/user.js
@@ -29,9 +29,10 @@ export async function editsUser(id, password) {
   const response = await request.put(`${process.env.PROD_URL}/users/${id}`)
   .send(password)
   return response.body;
+}
 
 export async function getByHighScore(){
   const response = await request.get(`${process.env.PROD_URL}/users/leaderboard`)
   return response.body
 }
-}
+
