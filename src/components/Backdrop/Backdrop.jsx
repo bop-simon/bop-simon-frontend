bec4e110cc6bcca0b3fc1595d33fcced358667ff
Katie Schrattenholzer <ktschr11@gmail.com>
2022-03-02 00:38:02 -0800

    adds tutorial modal, with drop in animation, to the home view. hides instructions button on click

---
+++ src/components/Backdrop/Backdrop.jsx
@@ -0,0 +1,15 @@
+import { motion } from "framer-motion"
+
+export default function Backdrop({ children, onClick }) {
+  return (
+    <motion.div
+      className='backdrop'
+      onClick={onClick}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      >
+      {children}
+    </motion.div>
+  )
+}
