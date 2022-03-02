3e2ecf70dc391af913675c25a08bd9c126e0f9e9
Katie Schrattenholzer <ktschr11@gmail.com>
2022-03-02 09:18:13 -0800

    tutorial styling

--- src/components/Backdrop/Backdrop.jsx
+++ src/components/Backdrop/Backdrop.jsx
@@ -1,9 +1,10 @@
 import { motion } from "framer-motion"
+import styles from './backdrop.module.css'
 
 export default function Backdrop({ children, onClick }) {
   return (
     <motion.div
-      className='backdrop'
+      className={styles.backdrop}
       onClick={onClick}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
