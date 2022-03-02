f916e4bbaa13fc4da2229e0bdcf36423c82ee657
EmilyDSarani <emily.sarani@gmail.com>
2022-03-02 16:23:32 -0600

    aboutme update

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,5 +1,5 @@
 import MenuBar from '../../components/MenuBar/MenuBar'
-import styles from './about.css'
+import styles from './about.module.css'
 import simon from '../../assets/simon.png'
 import sarani from '../../assets/OctogonWHeadphones.png'
 import julius from '../../assets/PentagonWHeadphones.png'
@@ -21,7 +21,7 @@ export default function About() {
             className={styles.simon}
           />
           <h2>Bop Simon</h2>
-          <hr></hr>
+          <br></br>
           <p>I hail from the land where music runs free and contenders attempt to take my top spot. My favorite song is too complicated for the human mind to comprehend. It is a series of beeps and boops that only musical geniuses can comprehend. My favorite thing about this app is me.</p>
           </div>
          <br></br>
@@ -33,8 +33,8 @@ export default function About() {
             className={styles.simon}
           />
           <h2>Taylor</h2>
-          <hr></hr>
-          <p>Chase ball of string eat plants, meow, and throw up because I ate plants going to catch the red dot today going to catch the red dot today. I could pee on this if I had the energy. <a href='https://www.linkedin.com/in/taylor-c-williams/'>LinkedIn</a> // <a href='https://github.com/taylor-c-williams'>Github</a></p>
+          <br></br>
+          <p>Chase ball of string eat plants, meow, and throw up because I ate plants going to catch the red dot today going to catch the red dot today. I could pee on this if I had the energy. <a className={styles.link} href='https://www.linkedin.com/in/taylor-c-williams/'>LinkedIn</a> // <a className={styles.link} href='https://github.com/taylor-c-williams'>Github</a></p>
           </div>
          <br></br>
           <div className={styles.team}>
@@ -45,8 +45,8 @@ export default function About() {
             className={styles.simon}
           />
           <h2>Katie</h2>
-          <hr></hr>
-          <p>Meow. Tuxedo cats always looking dapper hunt anything that moves, but grass smells good playing with balls of wool attack feet, yet chase ball of string. <a href='https://www.linkedin.com/in/k-schrattenholzer/'>LinkedIn</a> // <a href='https://github.com/k-schrattenholzer'>Github</a></p>
+          <br></br>
+          <p>Meow. Tuxedo cats always looking dapper hunt anything that moves, but grass smells good playing with balls of wool attack feet, yet chase ball of string. <a className={styles.link} href='https://www.linkedin.com/in/k-schrattenholzer/'>LinkedIn</a> // <a className={styles.link} href='https://github.com/k-schrattenholzer'>Github</a></p>
           </div>
          <br></br>
           <div className={styles.team}>
@@ -57,8 +57,8 @@ export default function About() {
             className={styles.simon}
           />
           <h2>Julius</h2>
-          <hr></hr>
-          <p>Intently stare at the same spot need to chase tail, so sit on human they not getting up ever eat the rubberband a nice warm laptop for me to sit on but cat ass trophy. Catch small lizards, bring them into house, then unable to find them on carpet. <a href='https://www.linkedin.com/in/juliusfranklin88/'>LinkedIn</a> // <a href='https://github.com/coding-neophyte'>Github</a></p>
+          <br></br>
+          <p>Intently stare at the same spot need to chase tail, so sit on human they not getting up ever eat the rubberband a nice warm laptop for me to sit on but cat ass trophy. Catch small lizards, bring them into house, then unable to find them on carpet. <a className={styles.link} href='https://www.linkedin.com/in/juliusfranklin88/'>LinkedIn</a> // <a className={styles.link} href='https://github.com/coding-neophyte'>Github</a></p>
           </div>
          <br></br>
           <div className={styles.team}>
@@ -69,8 +69,8 @@ export default function About() {
             className={styles.simon}
           />
           <h2>Sarani</h2>
-          <hr></hr>
-          <p>My favorite thing about this app was learning how tonejs works and how to utilize it. studying the game logic was a lot of fun and planning out and studying it was amazing. It was a lot of fun playing with different notes and I am glad that this team incorporated a free-play that allows users to join in on the fun. <a href='https://www.linkedin.com/in/emily-sarani/'>LinkedIn</a> // <a href='https://github.com/EmilyDSarani'>Github</a> </p>
+          <br></br>
+          <p>My favorite thing about this app was learning how tonejs works and how to utilize it. studying the game logic was a lot of fun and planning out and studying it was amazing. It was a lot of fun playing with different notes and I am glad that this team incorporated a free-play that allows users to join in on the fun. <a className={styles.link} href='https://www.linkedin.com/in/emily-sarani/'>LinkedIn</a> // <a className={styles.link} href='https://github.com/EmilyDSarani'>Github</a> </p>
           </div>
         </div>
       </section>
