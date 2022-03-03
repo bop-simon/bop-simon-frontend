3d8686a53c0633016036e1df67a33e2ea37727f9
EmilyDSarani <emily.sarani@gmail.com>
2022-03-02 19:01:49 -0600

    working random notes

--- src/utils/Gameplay/gamelogic.js
+++ src/utils/Gameplay/gamelogic.js
@@ -7,12 +7,11 @@ function randomLetter() {
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
 
-//I am not sure if to.String is going to be the best method
+
 export function getRandomNote(){
     const octave = randomNumber().toString()
     const key = randomLetter()
     return key + octave
 }
-
 
 
