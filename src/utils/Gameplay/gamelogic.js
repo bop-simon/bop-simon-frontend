c7bcd2e55d373913d937f0899a1adebdc4d18bdc
EmilyDSarani <emily.sarani@gmail.com>
2022-03-02 19:20:53 -0600

    testing getcurrentsong

--- src/utils/Gameplay/gamelogic.js
+++ src/utils/Gameplay/gamelogic.js
@@ -1,4 +1,4 @@
-export function randomNumber() { 
+function randomNumber() { 
     return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 }
 
@@ -7,11 +7,25 @@ function randomLetter() {
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
 
-
 export function getRandomNote(){
     const octave = randomNumber().toString()
     const key = randomLetter()
     return key + octave
 }
+//level 1 = 5 notes
+//currentSong.length = level * 5
+export function getCurrentSong(level){
+    let currentSong = []
+    const songLength = level * 5
+    
+    for(let i=0; i < songLength; i++){
+    const note = getRandomNote()
+    currentSong.push(note)
 
+    while(i < songLength){
+        currentSong.push(note)
+        }
+    }
+}
 
+console.log('soooong', getCurrentSong(2))
--- src/utils/Gameplay/gameplay.jsx
+++ src/utils/Gameplay/gameplay.jsx
@@ -1,23 +1,11 @@
-import { PlayLessonTwoTone } from '@mui/icons-material'
 import { useEffect } from 'react'
 import { useUser } from '../../context/UserContext'
-import { getRandomNote } from './gamelogic'
+import { getRandomNote, getCurrentSong } from './gamelogic'
 
 const {user, setUser}= useUser()
 let playerHistory = []
 let cpuHistory = []
 
-
-
-export function getCurrentSong(level){
-    let currentSong = []
-    const note = getRandomNote()
-    currentSong.push(note)
-    setUser(level)
-    user ? {level} : 0
-   //we want this to happen for 2 songs
-
-}
 
 useEffect(() => {
    if(playerHistory === cpuHistory){
